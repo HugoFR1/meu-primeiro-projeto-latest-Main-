ffc26484daaa566c446e7136a6d9bbd3bd5c9af9
Hugo Fraga <huguinhofraga@gmail.com>
2024-09-20 17:51:42 -0300

    Adicionando flow-control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,29 +6,24 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NewComponent, TemplateBindingComponent, TemplateVariablesComponent],
-  styles:[
-    `
-      h1 {
-        color: red;
-      }
-    `],
+  imports: [RouterOutlet, CommonModule, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent],
+
   template: `
   <!-- <app-template-binding/> -->
   <!--router-outlet></!--router-outlet> -->
-  <app-template-variables/>
+  <!-- <app-template-variables/> -->
   <h1>Curso de angular</h1>
 
+  <app-template-control-flow />
 
+`,
 
-  `,
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'meu-primeiro-projeto-latest';
 }
-//
+
---
+++ src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -0,0 +1,30 @@
+import { AsyncPipe, CommonModule, NgFor, NgIf, NgIfContext } from '@angular/common';
+import { Component, TemplateRef } from '@angular/core';
+import { delay, Observable, of } from 'rxjs';
+
+@Component({
+  selector: 'app-template-control-flow',
+  standalone: true,
+  imports: [AsyncPipe, NgIf, NgFor, CommonModule],
+  templateUrl: './template-control-flow.component.html',
+  styleUrl: './template-control-flow.component.scss'
+})
+export class TemplateControlFlowComponent {
+  public isTrue = true;
+
+  public itens = [{ name: 'Hugo Fraga' }];
+  public loadingData$: Observable<string[]> = of([
+    'item 1',
+    'item 2',
+    'item 3',
+  ]).pipe(delay(3000));
+name: any;
+
+public trackByFn(index: number){
+  return index;
+}
+
+public addNewName(value: string){
+  return this.itens.push({ name: value });
+  }
+}
