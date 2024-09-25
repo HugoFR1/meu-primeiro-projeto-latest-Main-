76a4880cb6f681a465291a7d81d7ce0a5d88e9f2
Hugo Fraga <huguinhofraga@gmail.com>
2024-09-25 17:50:09 -0300

    Criação template-deferrable-views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,22 +7,22 @@ import { NewComponent } from './components/new-component/new-component.component
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent],
+  imports: [RouterOutlet, CommonModule, NewComponent, TemplateBindingComponent, TemplateVariablesComponent, TemplateControlFlowComponent, TemplateDeferrableViewsComponent],
 
   template: `
   <!-- <app-template-binding/> -->
   <!--router-outlet></!--router-outlet> -->
   <!-- <app-template-variables/> -->
+  <!-- <app-template-control-flow /> -->
+
   <h1>Curso de angular</h1>
-
-  <app-template-control-flow />
-
+  <app-template-deferrable-views />
 `,
-
 })
 export class AppComponent {
 }
